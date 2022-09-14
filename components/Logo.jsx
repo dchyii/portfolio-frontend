6c257b8f30cdb348ac1954f1f618967e49b69730
dchyii <hongyichong@gmail.com>
2022-09-14 12:34:41 +0800

    created logo

---
+++ components/Logo.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="group relative w-48 h-48 overflow-hidden border border-green-500">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className=" w-full h-full text-red-500 absolute top-48 group-hover:top-0 group-hover:text-red-700 transition-all "
+      >
+        <path
+          fillRule="evenodd"
+          d="M11.47 7.72a.75.75 0 011.06 0l7.5 7.5a.75.75 0 11-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 01-1.06-1.06l7.5-7.5z"
+          clipRule="evenodd"
+        />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className=" w-full h-full text-red-500 absolute bottom-0 group-hover:bottom-48 group-hover:text-red-100 transition-all "
+      >
+        <path
+          fillRule="evenodd"
+          d="M11.47 7.72a.75.75 0 011.06 0l7.5 7.5a.75.75 0 11-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 01-1.06-1.06l7.5-7.5z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default Logo;
