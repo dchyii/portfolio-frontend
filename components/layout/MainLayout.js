67a84e024b298e6d821b356b413a3276faf47fbb
dchyii <hongyichong@gmail.com>
2022-09-21 13:12:11 +0800

    completed main layout

--- components/layout/MainLayout.js
+++ components/layout/MainLayout.js
@@ -4,7 +4,9 @@ export default function MainLayout({ children }) {
   return (
     <>
       <NavBar />
-      <main>{children}</main>
+      <div className="w-full lg:w-5/6 xl:w-4/5 h-screen mx-auto pt-20 pb-6 px-5 overflow-scroll">
+        <main>{children}</main>
+      </div>
     </>
   );
 }
