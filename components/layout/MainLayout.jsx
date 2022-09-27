4422fcafe774ee707f7c815d4ad544959d0b1f35
dchyii <hongyichong@gmail.com>
2022-09-27 12:33:50 +0800

    resolve logo location for import

--- components/layout/MainLayout.jsx
+++ components/layout/MainLayout.jsx
@@ -1,4 +1,4 @@
-import Logo from "../logo";
+import Logo from "../Logo";
 import NavBar from "./NavBar";
 
 export default function MainLayout({ children }) {
--- components/layout/NavBar.jsx
+++ components/layout/NavBar.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
-import Logo from "../logo";
+import Logo from "../Logo";
 
 const NavBar = () => {
   const router = useRouter();
