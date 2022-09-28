e686dff307e5bd5482b61c8bce97959cbb860861
Desmond Chong <desmond@90.136.189.118.static.m1net.com.sg>
2022-09-28 12:40:01 +0800

    changed colour scheme and border of sidebar

--- components/layout/SideBar.jsx
+++ components/layout/SideBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SideBar = () => {
   return (
-    <div className="w-full lg:w-24 h-16 lg:h-full grid grid-cols-4 lg:grid-cols-1 justify-items-center border-4 border-orange-300/0 border-t-orange-300 lg:border-t-orange-300/0 lg:border-r-orange-300">
+    <div className="w-full lg:w-24 h-16 lg:h-full grid grid-cols-4 lg:grid-cols-1 justify-items-center border-2 border-slate-100/0 border-t-slate-100/50 lg:border-t-slate-100/0 lg:border-r-slate-100">
       <div className="aspect-square w-16">1</div>
       <div className="aspect-square w-16">2</div>
       <div className="aspect-square w-16">3</div>
