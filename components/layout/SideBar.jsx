6295aa2f3214936f4f4c19074f94fe9dc65e0cb6
Desmond Chong <desmond@90.136.189.118.static.m1net.com.sg>
2022-10-04 12:56:08 +0800

    completed side bar with icons

--- components/layout/SideBar.jsx
+++ components/layout/SideBar.jsx
@@ -4,10 +4,10 @@ import SideBarIcon from "../subcomponents/SideBarIcon";
 const SideBar = () => {
   return (
     <div className="w-full lg:w-24 h-16 lg:h-full grid grid-cols-4 lg:grid-cols-1 justify-items-center border-2 border-slate-100/0 border-t-slate-100/50 lg:border-t-slate-100/0 lg:border-r-slate-100">
-      <SideBarIcon />
-      <SideBarIcon />
-      <SideBarIcon />
-      <SideBarIcon />
+      <SideBarIcon name={"profile"} />
+      <SideBarIcon name={"languages"} />
+      <SideBarIcon name={"projects"} />
+      <SideBarIcon name={"contact"} />
     </div>
   );
 };
--- components/subcomponents/SideBarIcon.jsx
+++ components/subcomponents/SideBarIcon.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import SVGIcon from "./SVGIcon";
 
-const SideBarIcon = () => {
+const SideBarIcon = (props) => {
   return (
     <div className="group relative h-fit mt-3 flex items-start hover:cursor-pointer">
       <svg
@@ -11,17 +12,13 @@ const SideBarIcon = () => {
         stroke="currentColor"
         className="w-10 md:w-12 lg:w-16 aspect-square hover:text-orange-500 z-50 bg-white"
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-        />
+        <SVGIcon icon={props.name} />
       </svg>
       <div
         className="absolute left-0 lg:group-hover:left-16 ml-2 px-3 py-3 rounded-r-lg bg-white min-w-max text-xl font-semibold border border-l-4 border-l-white border-slate-300
       opacity-0 lg:group-hover:opacity-100 invisible lg:group-hover:visible transition-all duration-500 ease-in-out"
       >
-        icon name
+        <p>{props.name}</p>
       </div>
     </div>
   );
