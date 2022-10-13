edf09c0dd98987814633983329a4405d79192782
Desmond Chong <desmond@90.136.189.118.static.m1net.com.sg>
2022-10-13 13:22:02 +0800

    added CSS for hero

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -3,19 +3,8 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="h-full snap-start pt-12 lg:pt-36 lg:flex lg:flex-row-reverse">
-      <div className="w-3/4 lg:w-2/5 mx-auto">
-        {/* image placeholder for profile pic */}
-        <Image
-          src="https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
-          alt="profile"
-          width={320}
-          height={320}
-          layout="responsive"
-          className="rounded-full"
-        />
-      </div>
-      <div className="mt-16 max-w-2xl h-fit items-end border border-yellow-500">
+    <div className="h-full pb-6 snap-start flex flex-col-reverse border border-green-500">
+      <div className="h-fit max-w-5xl border border-yellow-500">
         <h3 className="text-xl font-semibold font-mono">Hello World</h3>
         <h1 className="text-3xl font-semibold py-3">
           I am{" "}
@@ -27,6 +16,17 @@ const Hero = () => {
           I am a full stack developer, building tools to bring improvement to
           our everyday lives.
         </h2>
+      </div>
+      <div className="mt-6 mx-auto max-w-full sm:max-w-md lg:max-w-xl grow aspect-square border border-orange-500">
+        {/* image placeholder for profile pic */}
+        <Image
+          src="https://images.unsplash.com/photo-1592194996308-7b43878e84a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8"
+          alt="profile"
+          width={160}
+          height={160}
+          layout="responsive"
+          className="rounded-full"
+        />
       </div>
     </div>
   );
